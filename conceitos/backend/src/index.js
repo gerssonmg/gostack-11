dfaf074c7995e8ae11560705be77537d61763099
Gerson <gersoncafilho@gmail.com>
2020-04-13 07:22:56 -0300

    feat(index.js): add lib. Windows + . >> add emoji

--- conceitos/backend/src/index.js
+++ conceitos/backend/src/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+  return response.json({ message: 'Hello World!' });
 })
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('✔🚀💚🤍🕳🕎 Back-end started!');
+});
 
