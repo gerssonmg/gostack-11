9afb585a040887e136bf00282e68b35a984d0a5f
Gerson <gersoncafilho@gmail.com>
2020-04-19 19:39:52 -0300

    feat: method GET

--- conceitos/backend/src/index.js
+++ conceitos/backend/src/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' });
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2'
+  ]);
 })
 
 app.listen(3333, () => {
